08587fba754d44f0f6f5b61be34dff42c6d86fcc
Mike Lin <95307563+Maiku3@users.noreply.github.com>
2023-11-12 11:55:27 -0500

    deleted files

--- index.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from flask import render_template, request
-from predict import predict_sentiments
-from YTComments import get_video_comments
-from flask_cors import CORS
-import requests
-
-    
